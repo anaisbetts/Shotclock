330d751aafc573031d30bb5dbcf7cfd14ac3e113
Paul Betts <paul@paulbetts.org>
2012-06-15 17:45:58 -0700

    Fixup the test project

---
+++ Shotclock.Tests/ViewModels/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ReactiveUI;
+using Xunit;
+
+namespace Shotclock.Tests.ViewModels
+{
+    public class MainViewModelTests : IEnableLogger
+    {
+    }
+}
